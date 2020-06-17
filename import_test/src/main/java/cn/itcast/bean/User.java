f2f07f1abe1d6806cacfa151b5d42bb296525120
ytjl <896702900@qq.com>
2020-06-17 10:52:28 +0800

    city4

--- import_test/src/main/java/cn/itcast/bean/User.java
+++ import_test/src/main/java/cn/itcast/bean/User.java
@@ -9,7 +9,7 @@ import lombok.Data;
 public class User {
 	private String username;
 	private String money;
-	public String city3;
+	public String city4;
 	private Integer number;
 	private Integer age;
 	private String house;

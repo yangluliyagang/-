fa10844e70539f678fe2f920734a4eede5b8fa94
ytjl <liyagang_coder@163.cm>
2020-06-17 16:35:30 +0800

    我去

--- import_test/src/main/java/cn/itcast/bean/User.java
+++ import_test/src/main/java/cn/itcast/bean/User.java
@@ -10,9 +10,7 @@ public class User {
 	private String username1;
 	private String money1;
 	public String city4;
-	//要发生冲突
 	private Integer aaaaa;
-	//新用户测试
 	private Integer ddd;
 	private String eee;
 	private String jiedao6;
